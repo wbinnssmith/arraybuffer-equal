086129555772701b1f265a412921728657097d94
Will Binns-Smith <wbinnssmith@gmail.com>
2015-10-05 16:35:11 -0700

    semistandard

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-module.exports = function arraybufferEqual(buf1, buf2) {
-	if (buf1.byteLength !== buf2.byteLength) {
-		return false;
-	}
+module.exports = function arraybufferEqual (buf1, buf2) {
+  if (buf1.byteLength !== buf2.byteLength) {
+    return false;
+  }
 
-	var view1 = new DataView(buf1);
-	var view2 = new DataView(buf2);
+  var view1 = new DataView(buf1);
+  var view2 = new DataView(buf2);
 
-	var i = buf1.byteLength;
-	while (i--) {
-		if (view1.getUint8(i) !== view2.getUint8(i)) {
-			return false;
-		}
-	}
+  var i = buf1.byteLength;
+  while (i--) {
+    if (view1.getUint8(i) !== view2.getUint8(i)) {
+      return false;
+    }
+  }
 
-	return true;
+  return true;
 };
